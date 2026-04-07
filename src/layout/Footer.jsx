0ab081cfe3b7ee31bf0d7c5805f326609904c45a
Ronilyn <ronilynabril215@gmail.com>
2026-04-07 11:04:33 +0300

    Update portfolio

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const socialLinks = [
-  { icon: Github, href: "https:github.com/dimitrova-gif", label: "Github"},
+  { icon: Github, href: "https://github.com/dimitrova-gif", label: "Github"},
   { icon: Linkedin, href: "https://www.linkedin.com/in/ronilyn-abril-6759341b1/", label: "Linkedin"},
   { icon: Mail, href: "mailto:ronilyn.dev@gmail.com", label: "Mail"},
 ];
